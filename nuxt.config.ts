13ec72aed74b22f24a8a373a3cd082f12d8b947b
Jorge Amado <darklink1302@gmail.com>
2023-02-27 01:04:00 -0400

    Add register manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,17 @@ export default defineNuxtConfig({
       }]
     }
   },
+  routeRules: {
+    '/manifest.webmanifest': {
+      headers: {
+        'Content-Type': 'application/manifest+json',
+        'Cache-Control': 'public, max-age=0, must-revalidate'
+      }
+    }
+  },
   pwa: {
     installPrompt: true,
+    registerWebManifestInRouteRules: true,
     registerType: 'autoUpdate',
     workbox: { navigateFallback: null },
     devOptions: {
